bb64e9b0dc319b8f6e252fd60119b0d7325c0236
Esteban Rodriguez <erodriguezg7@gmail.com>
2022-04-24 14:54:52 -0500

    Paso 5: Agregando nuevo route /launchX

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000 // puerto localhost 3000
 
+// localhost:3000
 app.get("/", (req, res) =>{
 
     res.send("Hola Explorers!");
+})
+
+// localhost:3000/launchx
+app.get("/launchx", (req, res) =>{
+
+    res.send("Bienvenidos a LaunchX!");
 })
 
 app.listen(port, ()=>{
